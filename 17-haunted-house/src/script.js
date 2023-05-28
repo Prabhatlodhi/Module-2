8fc427599aab371b5d4af353264027eb619eaf5a
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-05-28 06:04:47 +0530

    aa

--- 17-haunted-house/src/script.js
+++ 17-haunted-house/src/script.js
@@ -14,21 +14,119 @@ const canvas = document.querySelector('canvas.webgl')
 // Scene
 const scene = new THREE.Scene()
 
+//Fog
+const fog = new THREE.Fog('#262837', 1,15)
+scene.fog = fog
+
 /**
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const doorColorTexture = textureLoader.load('/textures/door/color.jpg')
+const doorAlphaTexture = textureLoader.load('/textures/door/alpha.jpg')
+const doorAmbientOcclusionTexture = textureLoader.load('/textures/door/ambientOcclusion.jpg')
+const doorHeightTexture = textureLoader.load('/textures/door/height.jpg')
+const doorNormalTexture = textureLoader.load('/textures/door/normal.jpg')
+const doorMetalnessTexture = textureLoader.load('/textures/door/metalness.jpg')
+const doorRoughnessTexture = textureLoader.load('/textures/door/roughness.jpg')
 
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+
+//Group
+const house = new THREE.Group()
+scene.add(house)
+
+//walls
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(4,2.5,4),
+    new THREE.MeshStandardMaterial({color: 'red'})
 )
-sphere.position.y = 1
-scene.add(sphere)
+walls.position.y = 2.5 / 2
+    house.add(walls)
+
+//Roof
+const roof  = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5,1,4),
+    new THREE.MeshStandardMaterial({color:"yellow"})
+)
+// = 2.5(walls + half of roof one )
+roof.position.y = 2.5 + 0.5
+roof.rotation.y = Math.PI*0.25
+house.add(roof)
+
+//Door
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2.2,2.2,100,100),
+    new THREE.MeshStandardMaterial({
+        map:doorColorTexture,
+        alphaMap:doorAlphaTexture,
+        aoMap:doorAmbientOcclusionTexture,
+        displacementMap:doorHeightTexture,
+        displacementScale:0.1,
+        normalMap:doorNormalTexture,
+        metalnessMap:doorMetalnessTexture,
+        roughnessMap:doorRoughnessTexture
+    })
+)
+
+door.geometry.setAttribute('uv2', new THREE.Float32BufferAttribute(door.geometry.attributes.uv.array,2))
+//half of wall
+door.position.y = 1
+door.position.z = 2.001
+house.add(door)
+
+//Bushes
+const bushGeometry = new THREE.SphereGeometry(1,16,16)
+const bushMaterial = new THREE.MeshStandardMaterial({color:'#89c854'})
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial) 
+bush1.scale.set(0.5,0.5,0.5)
+bush1.position.set(3.8,0.2,2.5)
+house.add(bush1)
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial) 
+bush2.scale.set(0.5,0.5,0.5)
+bush2.position.set(2.8,0.2,2.5)
+house.add(bush2)
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial) 
+bush3.scale.set(0.5,0.5,0.5)
+bush3.position.set(0.8,0.2,2.5)
+house.add(bush3)
+
+//Graves
+const graves = new THREE.Group()
+scene.add(graves)
+
+const graveGeometry = new THREE.BoxGeometry(0.6,0.8,0.2)
+const graveMaterial  = new THREE.MeshStandardMaterial({color:'#yellow'})
+
+for (let i = 0; i < 50; i++) {
+    const angle = Math.random() * Math.PI * 2;
+    const radius = 3 + Math.random() * 6;
+    const x = Math.sin(angle) * radius;
+    const z = Math.cos(angle) * radius;
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial);
+    grave.position.set(x, 0.3, z);
+    grave.rotation.y = (Math.random()-0.5) * 0.4
+    grave.rotation.z = (Math.random()-0.5) * 0.4
+    graves.add(grave);
+  }
+
+
+
+
+
+
+
+
+// Temporary sphere
+// const sphere = new THREE.Mesh(
+//     new THREE.SphereGeometry(1, 32, 32),
+//     new THREE.MeshStandardMaterial({ roughness: 0.7 })
+// )
+// sphere.position.y = 1
+// scene.add(sphere)
+
 
 // Floor
 const floor = new THREE.Mesh(
@@ -43,7 +141,7 @@ scene.add(floor)
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.01)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
@@ -55,6 +153,12 @@ gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(moonLight)
+
+
+//DoorLight
+const doorLight = new THREE.PointLight('#ff7d46',1,7)
+doorLight.position.set(0,2.2,2.7)
+house.add(doorLight)
 
 /**
  * Sizes
@@ -101,7 +205,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
+renderer.setClearColor('#262837')
 /**
  * Animate
  */
